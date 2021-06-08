7c8f85e1a192e9f455de62aa665138f697d0d5c1
T.Soundarrajan <tcesoundar@gmail.com>
2021-06-08 23:51:18 +0530

    [Soundar] Add. Home Page for Quiz Portal

---
+++ src/components/home/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className="card">
+            <div className="card-content">
+                <div className="content">
+                    <h1>Start the Quiz</h1>
+                    <p>All the Best</p>
+                    <button className="button is-medium is-info">Start Quiz</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
